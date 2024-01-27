e2bba9062cbebcae19abc207067a397684150a5e
Ken Dahana <izwar.jati@gmail.com>
2024-01-27 08:29:59 +0700

    readded files

---
+++ Assets/Scripts/SpotlightPlayer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.CorgiEngine;
+using UnityEngine;
+
+public class SpotlightPlayer : MonoBehaviour
+{
+    Light spotlight;
+    bool isSpawned = false;
+    Character chare;
+    private void Update()
+    {
+        if (!isSpawned)
+        {
+            spotlight = GetComponent<Light>();
+            Character[] chars = FindObjectsOfType<Character>();
+            for (int i = 0; i < chars.Length; i++)
+            {
+
+                if (chars[i].CharacterType == Character.CharacterTypes.Player)
+                {
+                    chare = chars[i];
+                    Debug.Log(chars[i].transform.name);
+                    isSpawned = true;
+
+                }
+
+
+            }
+
+        }
+        spotlight.transform.LookAt(chare.transform);
+    }
+}
